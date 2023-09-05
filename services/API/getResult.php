94ec417942c88eada70e0e8cffc868ec39009d40
arehman5110 <a.rehman5110@gmail.com>
2023-09-05 17:27:31 +0500

    create api

--- services/API/getResult.php
+++ services/API/getResult.php
@@ -20,9 +20,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         $stmt = $pdo->prepare($_POST['qury']);
         $stmt->execute();
+
+        $record = '';
+        if ($_POST['function_name'] === "GetData") {
+            $record = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            
+            
+        }
+
+        
+
+        $response = array(
+            'success' => true,
+            'status' => 200,
+            'data'=>$record
+        );
+        echo json_encode($response);
+        exit;
+
         
     } catch (PDOException $e) {
-        //throw $th;
+        $response = array(
+            'success' => false,
+            'status' => 500,
+            'error' => $e->getMessage(),
+        );
+        echo json_encode($response);
+        exit;
     }
 
 
